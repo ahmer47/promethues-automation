564eb9d7e3530ecfdefe83afdab7a152e97e46cd
Ahmer <ahmer@webook.com>
2024-07-10 13:12:23 +0300

    changes

--- installer.sh
+++ installer.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+
 prometheus_version="2.53.0"
 blackbox_exporter_version="0.25.0"
 node_exporter_version="1.8.1"
+
 monitor_count=0
 count=1
 Monitor_Final=""
 prefix=$"        - "
 postfix=$"\n"
+
 declare -a monitor_names
 echo "How many http monitors you want to install? (1-100)"
 read monitor_count
@@ -26,11 +29,11 @@ read monitor_count
 	for i in "${monitor_names[@]}"
 	do
 	   : 
-	   echo -e "$prefix$i" >> monitors.txt
+	   echo -e "$prefix$i" >> tempData/monitors.txt
 	done
-	sed -i '18r monitors.txt' finalprom
+	sed -i '18r tempData/monitors.txt' conf/prometheus.yml
 	echo -e "\n\n"
-	echo "Setting Prometheus permissions"
+	echo "Setting Prometheus Configuration"
 	sleep 3
 	echo -e "\n"
 
@@ -61,9 +64,15 @@ read monitor_count
 	
 	tar -xf prometheus-${prometheus_version}.linux-amd64.tar.gz
 
+	echo -e "\n\n"
+	echo "Copying Prometheus binaries"
+
 	cp prometheus-${prometheus_version}.linux-amd64/prometheus /usr/local/bin/
 
 	cp prometheus-${prometheus_version}.linux-amd64/promtool /usr/local/bin/
+
+	echo -e "\n\n"
+	echo "Setting Prometheus permissions"
 
 	chown prometheus:prometheus /usr/local/bin/prometheus
 
@@ -77,6 +86,9 @@ read monitor_count
 
 	chown -R prometheus:prometheus /etc/prometheus/console_libraries
 
+	echo -e "\n\n"
+	echo "Cleaning Prometheus left-over"
+
 	rm -rf prometheus-${prometheus_version}.linux-amd64*
 
 	echo -e "\n\n"
@@ -84,70 +96,78 @@ read monitor_count
 	sleep 3
 	echo -e "\n"
 
-#	wget https://github.com/prometheus/node_exporter/releases/download/v1.8.1/node_exporter-1.8.1.linux-amd64.tar.gz
 	wget https://github.com/prometheus/node_exporter/releases/download/v${node_exporter_version}/node_exporter-${node_exporter_version}.linux-amd64.tar.gz 
 
 	tar -xf node_exporter-${node_exporter_version}.linux-amd64.tar.gz
-
+	echo -e "\n\n"
+	echo "Setting Node Exporter binaries"
 	cp node_exporter-${node_exporter_version}.linux-amd64/node_exporter /usr/local/bin
+
+	echo -e "\n\n"
+	echo "Setting Node Exporter permissions"
 
 	chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
+	echo -e "\n\n"
+	echo "Cleaning Node Exporter left-over"
+
 	rm -rf node_exporter-${node_exporter_version}.linux-amd64*
 
-	cp prometheus.service /etc/systemd/system/prometheus.service
+	cp services/prometheus.service /etc/systemd/system/prometheus.service
 
-	cp node_exporter.service  /etc/systemd/system/node_exporter.service
+	cp services/node_exporter.service  /etc/systemd/system/node_exporter.service
 	
 	echo -e "\n\n"
 	echo "Installing BlackBox Monitor in 3 seconds"
 	sleep 3
 	echo -e "\n"
 	
-#	wget https://github.com/prometheus/blackbox_exporter/releases/download/v0.25.0/blackbox_exporter-0.25.0.linux-amd64.tar.gz
 	wget https://github.com/prometheus/blackbox_exporter/releases/download/v${blackbox_exporter_version}/blackbox_exporter-${blackbox_exporter_version}.linux-amd64.tar.gz
+	
+	echo -e "\n\n"
+	echo "Setting BlackBox Exporter binaries"
+
 	tar -xvf blackbox_exporter-${blackbox_exporter_version}.linux-amd64.tar.gz
 	
 	cp blackbox_exporter-${blackbox_exporter_version}.linux-amd64/blackbox_exporter /usr/local/bin/blackbox_exporter
-	
+	echo -e "\n\n"
+	echo "Setting BlackBox Exporter permissions"
 	chown blackbox_exporter:blackbox_exporter /usr/local/bin/blackbox_exporter
 	
 	rm -rf blackbox_exporter-${blackbox_exporter_version}.linux-amd64*
 	
 	mkdir /etc/blackbox_exporter
 	
-	cp blackbox.yml /etc/blackbox_exporter/blackbox.yml
+	cp services/blackbox.yml /etc/blackbox_exporter/blackbox.yml
 	
 	chown blackbox_exporter:blackbox_exporter /etc/blackbox_exporter/blackbox.yml
 	
 	cp blackbox_exporter.service /etc/systemd/system/blackbox_exporter.service
-	
+	cp conf/prometheus.yml  /etc/prometheus/prometheus.yml
+
+	echo -e "\n\n"
+	echo "Finalizing system services"
+
 	systemctl daemon-reload
 	sleep 2
-	systemctl start blackbox_exporter
-	sleep 2
+	systemctl enable prometheus
+	systemctl enable node_exporter	
 	systemctl enable blackbox_exporter
 	sleep 2
-	
-	cp finalprom  /etc/prometheus/prometheus.yml
 
-	sleep 2
+	echo -e "\n\n"
+	echo "Starting all services..."
+
 	systemctl start node_exporter
-	sleep 2
-	systemctl start prometheus
-	sleep 2
-	systemctl enable prometheus
-	sleep 2
-	systemctl enable node_exporter
-	sleep 2
+	systemctl start blackbox_exporter
 	systemctl restart prometheus
 	sleep 2
+
 	echo -e "\n\n"
-	echo "Finishing installation in few moments"
+	echo "Finishing installation in few moments..."
 	sleep 1
-	truncate -s 0 monitors.txt
+	truncate -s 0 tempData/monitors.txt
 	sleep 1
-	# cp finalprom-Backup finalprom
 	echo -e "\n\n"
 	echo "DONE"
 	echo -e "\n"
